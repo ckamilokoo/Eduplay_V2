49206ef24e9ebe8724d5934993cc499082b54c90
camilo campos <81344977+ckamilokoo@users.noreply.github.com>
2024-09-09 22:47:15 -0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -173,5 +173,5 @@ async def disconnect_all_devices():
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8080)
 
